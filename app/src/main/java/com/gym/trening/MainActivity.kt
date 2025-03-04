4130650c1f89613be2bd50dda872820e7ba48eca
refy1k <refy1k@ya.ru>
2025-03-04 14:26:34 +0500

    Added colors in Color.kt Modified MainActivity.kt Modified gitignore

--- app/src/main/java/com/gym/trening/MainActivity.kt
+++ app/src/main/java/com/gym/trening/MainActivity.kt
@@ -4,6 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.gym.trening.ui.theme.TreningTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,7 +19,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TreningTheme {
-
+                Row(Modifier.padding(30.dp, 20.dp)) {
+                    Text("tренировки",
+                        style = TextStyle(
+                            fontSize = 40.sp))
+                }
             }
         }
     }
--- app/src/main/java/com/gym/trening/ui/theme/Color.kt
+++ app/src/main/java/com/gym/trening/ui/theme/Color.kt
@@ -9,3 +9,12 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// Personal colors
+
+val Purple = Color(0xFF9580FF)
+val Pink = Color(0xFFE5A5FF)
+val Yellow = Color(0xFFFFCD95)
+val Orange = Color(0xFFFF8B8B)
+val DarkPurpleBlue = Color(0xFF36324A)
+val White = Color(0xFFD9D9D9)
